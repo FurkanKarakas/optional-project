5663c2da3a45f366eab70a0464c5f33fad829a8a
Furkan Karakas <furkan.karakas@epfl.ch>
2021-03-28 23:48:58 +0200

    Added Huffman compression. Added visualizations.

--- implementations.py
+++ implementations.py
@@ -1,8 +1,8 @@
 import numpy as np
-from typing import List, Tuple
+from typing import DefaultDict, Dict, List, Tuple, Union
+from collections import defaultdict
 
-
-np.set_printoptions(precision=10, suppress=True)  # type: ignore
+np.set_printoptions(precision=10, suppress=True)
 
 # The distinct features in our database which take the values from (0,1)
 FEATURES = ("COVID", "Flu", "Cold")
@@ -30,7 +30,7 @@ MAPPING_DICT_INDEXES = {
 }
 
 
-def generateDB(N: int, P: List[float]) -> List[int]:
+def generateDB(N: int, P: List[float]) -> np.ndarray:
     """Generates a database of size N.
 
     Args:
@@ -48,7 +48,7 @@ def generateDB(N: int, P: List[float]) -> List[int]:
     return D
 
 
-def LDP(D: List[int], R: List[float]) -> List[int]:
+def LDP(D: List[int], R: List[float]) -> np.ndarray:
     """Local differential privacy mechanism. Flips the bit `i` with a probability of `R[i]`.
 
     Args:
@@ -143,7 +143,7 @@ def LaplaceCountHistogram(D: List[int], epsilon: float = 1) -> Tuple[float, floa
     return queryResult
 
 
-def ExponentialMechanism(D: List[int], epsilon: float = 1) -> int:
+def ExponentialMechanism(D: np.ndarray, epsilon: float = 1) -> int:
     """Choose one of the more common tuples based on the counts
 
     Args:
@@ -157,25 +157,180 @@ def ExponentialMechanism(D: List[int], epsilon: float = 1) -> int:
     utilityScores = D
     # The sensitivity here is 1 since addition of 1 person can change the count at most 1
     sensitivity = 1
-    probabilities = [np.exp(epsilon*score/(2*sensitivity))
-                     for score in utilityScores]
+    probabilities = np.exp(epsilon*utilityScores/(2*sensitivity))
     probabilities /= np.sum(probabilities)
     choice = np.random.choice(8, 1, p=probabilities)
     choice = choice.item()
     return choice
 
+# A Huffman Tree Node
+
+
+class node:
+    def __init__(self, freq, symbol, left=None, right=None):
+        # frequency of symbol
+        self.freq = freq
+
+        # symbol name (character)
+        self.symbol = symbol
+
+        # node left of current node
+        self.left = left
+
+        # node right of current node
+        self.right = right
+
+        # tree direction (0/1)
+        self.huff = ''
+
+# Store the symbol-code pairs
+
+
+def storeNodes(node, symbolCodeDict, val=''):
+    # huffman code for current node
+    newVal = val + str(node.huff)
+
+    # if node is not an edge node
+    # then traverse inside it
+    if(node.left):
+        storeNodes(node.left, symbolCodeDict, newVal)
+    if(node.right):
+        storeNodes(node.right, symbolCodeDict, newVal)
+
+    # if node is edge node then display its huffman code
+    if(not node.left and not node.right):
+        symbolCodeDict[node.symbol] = newVal
+
+
+def HuffmanCompression(D: Union[np.ndarray, Dict[str, int]]) -> Tuple[Dict[str, str], DefaultDict[str, int]]:
+    """The Huffman compression algorithm which compresses the symbols "000" to "111" based on their frequencies.
+
+    Args:
+        D (Union[List[int], DefaultDict[str, int]]): Input database to be compressed. Can be either list or dictionary.
+
+    Returns:
+        Tuple(Dict[str, str], DefaultDict[str, int]: Huffman symbol-code mapping and compressed database in the output. It maps the Huffman code (str, e.g. "01") associated with a symbol to a number, which is the number of people in a particular condition.
+    """
+    compressedDB = defaultdict(int)
+    chars = freq = None
+    # If the database is a dictionary, process it appropriately
+    if isinstance(D, dict):
+        chars = list(D.keys())
+        freq = list(D.values())
+    # If the database is a numpy array, process it appropriately
+    else:
+        chars = [str(i) for i in np.arange(0, 8)]
+        freq = D
+
+    # list containing unused nodes
+    nodes = list()
+
+    # Converting characters and frequencies into huffman tree nodes
+    for x in range(len(chars)):
+        nodes.append(node(freq[x], chars[x]))
+
+    # We continue processing the nodes as long as there are more than 1 node in the list.
+    while len(nodes) > 1:
+        # Sort all the nodes in ascending order based on their frequency
+        nodes = sorted(nodes, key=lambda x: x.freq)
+
+        # pick 2 smallest nodes
+        left = nodes[0]
+        right = nodes[1]
+
+        # assign directional value to these nodes
+        left.huff = 0
+        right.huff = 1
+
+        # Combine the 2 smallest nodes to create new node as their parent
+        newNode = node(left.freq+right.freq, left.symbol +
+                       right.symbol, left, right)
+
+        # remove the 2 nodes and add their
+        # parent as new node among others
+        nodes.remove(left)
+        nodes.remove(right)
+        nodes.append(newNode)
+
+    # Huffman Tree is ready!
+    symbolCodeMapping = dict()
+    # Note that storeNodes traverses the binary tree in DFS manner. We stop at the leaf nodes and record the values in the dictionary.
+    storeNodes(nodes[0], symbolCodeDict=symbolCodeMapping)
+
+    # Return the new database
+    if isinstance(D, dict):
+        for symbol, code in symbolCodeMapping.items():
+            compressedDB[code] += D[symbol]
+    else:
+        for symbol, code in symbolCodeMapping.items():
+            compressedDB[code] += D[int(symbol)]
+
+    return symbolCodeMapping, compressedDB
+
+
+# Local differential privacy algorithm on a compressed databse
+
+
+def LDP_compressed(D: Dict[str, int], epsilon=.1):
+    """Local differential privacy applied on a compressed database D.
+
+    Args:
+        D (dict): A dictionary which maps Huffman-compressed symbols to patients
+        epsilon (float, optional): Probability to flip a bit. Defaults to .1.
+    """
+
+    assert epsilon <= 1, "The probability to flip a bit must be smaller than or equal to 1."
+
+    D_LDP = defaultdict(int)
+    for key, val in D.items():
+        # Decomposition of index into bits
+        bits = list()
+        for bit in key:
+            bits.append(int(bit))
+        for _ in range(val):
+            # The outcome 1 means "flip", the outcome 0 means "don't flip"
+            flips = list()
+            for bit in bits:
+                flip = np.random.binomial(1, epsilon)
+                flips.append(flip)
+            # The new index calculation
+            newIndex = "".join(str(abs(bits[i]-flips[i]))
+                               for i in range(len(bits)))
+            # Increase the count of the new entry
+            D_LDP[newIndex] += 1
+
+    return D_LDP
+
+
+def ComputeEntropy(D: Dict[str, int]):
+    """Compute the entropy of a given database
+
+    Args:
+        D (dict): Database in a dictionary format.
+    """
+
+    freq = list(D.values())
+    freq = np.array(freq)
+    # Normalize the probabilities
+    freq = freq/freq.sum()
+    # Remove the entries with 0 to avoid 0*log(0)
+    freq = freq[freq != 0]
+    entropy = freq*np.log2(freq)
+    entropy = entropy.sum()*-1
+    return entropy
+
 
 if __name__ == "__main__":
-    D = generateDB(10, [.0, .0, .0, .0, 1.0, .0, .0, .0])
-    print(D)
-    D_new = LDP(D, [1.0, .5, .5])
-    print(D_new)
-    D_laplace1 = LaplaceCount1(D)
-    print(D_laplace1)
-    D_laplace2 = LaplaceCount2(D, epsilon=100)
-    print(D_laplace2)
-    D_laplaceHist = LaplaceCountHistogram(D, epsilon=50)
-    print(D_laplaceHist)
-    mostCommonIndex = ExponentialMechanism(D)
-    print(
-        f"The most common index is {mostCommonIndex} which denotes {MAPPING_DICT[mostCommonIndex]}.")
+    # The mapping from the database index to the FEATURES space
+    # MAPPING_DICT = {
+    #    0: frozenset({}),
+    #    1: frozenset({"Cold"}),
+    #    2: frozenset({"Flu"}),
+    #    3: frozenset({"Flu", "Cold"}),
+    #    4: frozenset({"COVID"}),
+    #    5: frozenset({"COVID", "Cold"}),
+    #    6: frozenset({"COVID", "Flu"}),
+    #    7: frozenset({"COVID", "Flu", "Cold"}),
+    # }
+    D = generateDB(1000, [.26, .24, .1, .1, .2, .05, .04, .01])
+    print(ComputeEntropy({"0": 10, "1": 100}))
